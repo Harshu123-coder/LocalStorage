740b9f2d8ff0972daf0740cb36ea31bb71b3bacd
Harshu123-coder <harshuamule@gmail.com>
2023-08-02 10:01:30 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+// const newI = document.createElement('div');
+
+// newI.className="div3";
+// newI.id="div3";
+
+// const text = document.createTextNode( '');
+
+console.log(localStorage.setItem('username','harsha'));
+console.log(localStorage.setItem('email','harsha@gmail.com'));
+console.log(localStorage.setItem('password','harsha123'));
