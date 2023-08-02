80cfbfe8db6c0148c6818d36af440c0e0722577b
Harshu123-coder <75938652+Harshu123-coder@users.noreply.github.com>
2023-08-02 10:19:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,17 @@
 console.log(localStorage.setItem('username','harsha'));
 console.log(localStorage.setItem('email','harsha@gmail.com'));
 console.log(localStorage.setItem('password','harsha123'));
+
+let myobj ={
+    name: "hatsha",
+    age :22
+};
+
+let myobj_sting = JSON.stringify(myobj);
+console.log(myobj_sting);
+
+localStorage.setItem("myobj",myobj_sting);
+console.log(localStorage);
+
+let myobj_deserialized = JSON.parse(localStorage.getItem("myobj"));
+console.log(myobj_deserialized);
